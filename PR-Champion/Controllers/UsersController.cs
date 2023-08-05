a05b3b4c47b447ed8a5849c936b718036048e709
Conrad Kersten <67552218+connyIsTired@users.noreply.github.com>
2023-08-05 00:29:57 -0500

    initial commit

---
+++ PR-Champion/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PR_Champion.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetUsers()
+        {
+            return new JsonResult(
+                new List<object> {
+                    new {id = 1, name = "conrad"},
+                    new {id = 2, name = "bill"}
+                }
+                );
+        }
+    }
+}
